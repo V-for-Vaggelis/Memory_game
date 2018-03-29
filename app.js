54be59eeafe67d89de9b2ce9a2b8aa9389072b06
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-03-29 19:32:21 +0300

    Added modal

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ function myCode() {
    let matches = 0;
    let status = 0;
    let time = 0;
+   let modal = document.getElementById('myModal');
+   // Get the <span> element that closes the modal
+   let closeModal = document.getElementsByClassName("close")[0];
 
    function startTimer() {
      status = 1;
@@ -190,6 +193,19 @@ function countMove(numOfMoves) {
 
  function Won() {
    stopTimer();
+   modal.style.display = "block";
+   // When the user clicks on <span> (x), close the modal
+    closeModal.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
    /*deck.children().remove();
    const message = "<h1>Congratulations!!!</h1>"
    let stars;
