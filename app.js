4fda40995e379bebff8a939b78afba4d54230089
Evangelos Athanasakis <vaggelis.atha1993@gmail.com>
2018-03-31 00:20:24 +0300

    Added card-reject effect

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 "use strict";
 function myCode() {
+  const deck = $('.deck');
     /*
-   * Create a list that holds all of your cards
+   * Create a list that holds all of my cards
    */
-   const deck = $('.deck');
    let cards = document.getElementsByClassName("card");
    let cardsArray = [...cards];
-   let moves = 0;
-   let failedMoves =0;
+   let moves, failedMoves, matches, status, time = 0;
    let stars = $("#stars");
    let starCount = 3;
-   let matches = 0;
-   let status = 0;
-   let time = 0;
    let modal = document.getElementById('myModal');
    // Get the <span> element that closes the modal
    let closeModal = document.getElementsByClassName("close")[0];
+   // One array to save all the matches card and one to help me make the comparison
+   let matchedCards = [];
+   let  openCards = [];
 
+   // I'll create a timer that will lively show on the webpage as showed in this tutorial: https://www.youtube.com/watch?v=iSLWtGAw1Ic
    function startTimer() {
      status = 1;
      timer();
@@ -31,7 +31,6 @@ function myCode() {
      time = 0;
    }
 
-   // Stopwatch from: https://www.youtube.com/watch?v=gpFPppFU8s8
    function timer() {
      if (status === 1) {
        setTimeout(function() {
@@ -54,25 +53,25 @@ function myCode() {
      }
    }
 
+   // I start the stopwatch on pageload
    document.addEventListener("DOMContentLoaded", function() {
      startTimer();
    });
 
-  /*
-   * Display the cards on the page
-   *   - shuffle the list of cards using the provided "shuffle" method below
-   *   - loop through each card and create its HTML
-   *   - add each card's HTML to the page
-   */
+   // A function that restarts the game without page reload, it also shuffles the cards
    function restartGame() {
      let MoveShow = document.getElementById("moves");
      MoveShow.textContent = "0 Moves";
      moves = 0;
-     failedMoves =0;
+     failedMoves = 0;
      matches = 0;
      starCount = 3;
+     openCards = [];
+     matchedCards =[];
+     // I'll re-initialize the rating to be 3 stars
      stars.children().remove();
      stars.append("<li><i class=\"fa fa-star\"></i></li>\n<li><i class=\"fa fa-star\"></i></li>\n<li><i class=\"fa fa-star\"></i></li>");
+     // Shuffle the cards and replace them on the deck
      shuffle(cardsArray);
      deck.children().remove();
      let i = 0;
@@ -80,6 +79,8 @@ function myCode() {
        i += 1;
        card.classList.remove("open");
        card.classList.remove("show");
+       card.classList.remove("match");
+       // I add an id for each card it will help me later on
        card.setAttribute("id", "card" + i);
        deck.append(card);
      }
@@ -87,6 +88,7 @@ function myCode() {
      timer();
    }
 
+   // Also call this function on page load so that the shuffle happens anyway
    restartGame();
   // Shuffle function from http://stackoverflow.com/a/2450976
   function shuffle(array) {
@@ -103,22 +105,13 @@ function myCode() {
       return array;
   }
 
-
+  // When the restart button is clicked call the restartGame function
   const restart = $('.restart');
   restart.click(function() {
     restartGame();
   });
 
-  /*
-   * set up the event listener for a card. If a card is clicked:
-   *  - display the card's symbol (put this functionality in another function that you call from this one)
-   *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-   *  - if the list already has another card, check to see if the two cards match
-   *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-   *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-   *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-   *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-   */
+
    function cardShow(clickedCard) {
      clickedCard.classList.add("open");
      clickedCard.classList.add("show");
@@ -127,88 +120,99 @@ function myCode() {
    function cardHide(card) {
      card.classList.remove("open");
      card.classList.remove("show");
-
+     card.classList.remove("reject");
    }
 
-   function delay(card1, card2) {
+   // This function will create an effect for wron guesses
+   function redReject(card1, card2) {
+     setTimeout( function() {
+       card1.classList.add("reject");
+       card2.classList.add("reject");
+     }, 400);
+   }
+
+   // This function will make sure cards are visible before being closed after a wrong guess
+   function delayedHide(card1, card2) {
     setTimeout( function() {
       cardHide(card1);
       cardHide(card2);
-     }, 1000);
+    }, 1200);
   }
 
-   let openCards = [];
-
+  // A function to push a clicke dcard for comparison
    function addOpened(clickedCard) {
-     // let clickedCard = document.getElementById(CardId);
      openCards.push(clickedCard);
    }
 
-  let matchedCards = [];
-
+   // A function to store matched cards
   function addMatched(card) {
       matchedCards.push(card);
   }
 
-/*deck.click(function() {
-  Won();
-});*/
+  // lear right click menu inside the deck
+  deck.contextmenu(function(evt) {
+    evt.preventDefault();
+  });
+
+  // The event listener for clicking a card, this is where all the magic happens
   deck.on("click", ".card", function(evt) {
    let target = $(evt.target);
    let targetId = target.attr("id");
    let targetedEl = document.getElementById(targetId);
+   // I wrote two loops to prevent problems when user clicks an open card
    for (const card of matchedCards) {
-     let cardId = card.getAttribute("id");
-     if (cardId === targetId) {
-       return false;
-     }
+    let cardId = card.getAttribute("id");
+    if (cardId === targetId) {
+     return false;
+    }
    }
    for (const card of openCards) {
-     let cardId = card.getAttribute("id");
-     if (cardId === targetId) {
-       return false;
-     }
+    let cardId = card.getAttribute("id");
+    if (cardId === targetId) {
+     return false;
+    }
    }
+   // So if a card is opned or matched kill the handler, even though it works there might be some errors in dev tools
 
-
-   // let targetOpen = targetedEl.classList.contains("active");
-   // if (!targetOpen) {
-     cardShow(targetedEl);
-     addOpened(targetedEl);
-      let numOfOpen = openCards.length;
-      if (numOfOpen === 2) {
-        moves += 1;
-        countMove(moves);
-       let firstIcon = openCards[0].innerHTML;
-       let secondIcon = openCards[1].innerHTML;
-       if (firstIcon !== secondIcon) {
-         delay(openCards[0], openCards[1]);
-         failedMoves += 1;
-         rating(failedMoves);
-       }
-       else {
-         addMatched(openCards[0]);
-         addMatched(openCards[1]);
-         matches += 1;
-         // addMatched(openCards[0], openCards[1]);
-         if (matches === 8) {
-           Won();
-         }
-       }
-       openCards = [];
+  cardShow(targetedEl);
+  addOpened(targetedEl);
+  let numOfOpen = openCards.length;
+  if (numOfOpen === 2) {
+    moves += 1;
+    countMove(moves);
+    // If two card have been clicked I can compare them
+   let firstIcon = openCards[0].innerHTML;
+   let secondIcon = openCards[1].innerHTML;
+   if (firstIcon !== secondIcon) {
+    delayedHide(openCards[0], openCards[1]);
+    redReject(openCards[0], openCards[1]);
+    failedMoves += 1;
+    // Check if the user loses a star
+    rating(failedMoves);
+   }
+   else {
+    for (const card of openCards) {
+     card.classList.add("match");
+     addMatched(card);
+    }
+     matches += 1;
+     // If the user made 8 matches of 16 cards he has won
+     if (matches === 8) {
+       won();
      }
+    }
+     // After a comparison clear the array that stores the compared cards for next use
+     openCards = [];
+   }
+  });
 
-   // else {
-     // cardHide(targetedEl);
-     // openCards = [];
-     // return false;
- });
-
+// A function that counts every move and projects it on the website
 function countMove(numOfMoves) {
   let MoveShow = document.getElementById("moves");
   MoveShow.textContent = numOfMoves + " Moves";
 }
 
+// A function that lowers the rating after some wrong guesses
  function rating(numOfMistakes) {
    if ((numOfMistakes === 5)||(numOfMistakes === 8)) {
      stars.children().last().remove();
@@ -216,7 +220,10 @@ function countMove(numOfMoves) {
    }
  }
 
- function Won() {
+ /* A function that runs once the user wins. It shows the popup modal. It pulls the values of time and rating
+ and displays them to the user,
+ also gives to the user the ability to restart the game */
+ function won() {
    stopTimer();
    let rating = document.getElementById("stars").innerHTML;
    let ratingModal = document.getElementById("stars-modal");
@@ -244,24 +251,6 @@ function countMove(numOfMoves) {
             modal.style.display = "none";
         }
     }
-
-   /*deck.children().remove();
-   const message = "<h1>Congratulations!!!</h1>"
-   let stars;
-   let starsCode;
-   for (let i=0; i<starCount; i++) {
-     stars += "<li><i class=\"fa fa-star\"></i></li>\n";
-   }
-   starsCode = "<ul>\n" + stars + "</ul>";
-   let rating = "<h3>Your rating: </h3>";
-   let button = "<button type=\"button\">Play again!</button>";
-   deck.append(message);
-   deck.append(rating);
-   deck.append(starsCode);
-   deck.append(button);
-   // deck.innerHTML = "<h1> Congratulations!!!</h1>";
- }*/
   }
-
 }
 $(myCode());
